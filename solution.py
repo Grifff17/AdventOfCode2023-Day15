5478ccd3b5227466e4b8a1d121a45196c9482ac3
Griffin Riley <rileyg@oregonstate.edu>
2024-03-27 11:45:35 -0700

    solved part 1

--- solution.py
+++ solution.py
@@ -1,4 +1,17 @@
+def solvepart1():
+    data = fileRead("input.txt")
+    steps = data[0].strip().split(",")
 
+    sum = 0
+    for step in steps:
+        val = 0
+        for pos in step:
+            val = val + ord(pos)
+            val = val * 17
+            val = val % 256
+        #print(val)
+        sum = sum + val
+    print(sum)
 
 def fileRead(name):
     data = []
@@ -6,3 +19,5 @@ def fileRead(name):
     for line in f:
         data.append(line);
     return data
+
+solvepart1()
